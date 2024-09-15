dc4b984bac9dee0d0adf8a0660619d8d757f6507
T1it5an1icCSV <145016510+T1it5an1icCSV@users.noreply.github.com>
2024-09-15 18:55:48 +0300

    Add files via upload

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVC.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: TestController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // Другие действия (Details, Create, Edit, Delete) остаются без изменений
+    }
+}
